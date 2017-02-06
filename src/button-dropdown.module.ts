04884d04a7dec702d01ce78469536d9b78ccd9ee
Dariusz Filipiak <darekf77@gmail.com>
2017-02-06 13:18:04 +0100

    module

---
+++ src/button-dropdown.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ButtonDropdownComponent }   from './button-dropdown.component';
+
+@NgModule({
+    imports: [],
+    exports: [ButtonDropdownComponent],
+    declarations: [ButtonDropdownComponent],
+    providers: [],
+})
+export class ButtonDropdownModule { }
